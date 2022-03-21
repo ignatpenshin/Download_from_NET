08edc05f39211fb66c89852e5be27844f795f50b
Ignat Penshin <61110400+ignatpenshin@users.noreply.github.com>
2022-03-21 16:42:32 +0300

    Add files via upload
    add auto stitching for Insta360 One X2 with opticalflow

--- camera_download.py
+++ camera_download.py
@@ -2,10 +2,8 @@ from datetime import datetime, timezone
 import pysftp
 import sys
 import math
-import time
 import os
-# from multiprocessing import Process
-from subprocess import Popen, CREATE_NEW_CONSOLE, PIPE
+from subprocess import Popen
 
 
 def folder_creator():
@@ -51,8 +49,12 @@ def progressbar(x, y):
     sys.stdout.flush()
 
 
-def camera_process(basic_dir, Hostname, Username, Password, cnopts, \
+def camera_process(stitch_path, stitch_runner, basic_dir, Hostname, Username, Password, cnopts, \
                     remoteFilePath, remoteFilePath_phone, cameras, phone_list, cam_path):
+    created_tracks = []
+    # original_photos = []
+    # stitched_photos = []
+
     with pysftp.Connection(host=Hostname, username=Username, password=Password, cnopts=cnopts) as sftp:
         dir_to_create = ''
         cam_path_way = remoteFilePath + cam_path + "/DCIM"
@@ -66,6 +68,7 @@ def camera_process(basic_dir, Hostname, Username, Password, cnopts, \
                                                 and (x.filename.endswith('.insp') or x.filename.endswith('.jpg'))]:
                 file_date = datetime.fromtimestamp(file.st_mtime, tz=timezone.utc).strftime('%Y%m%d_%H%M%S')
                 total_date = datetime.fromtimestamp(file.st_mtime, tz=timezone.utc).strftime('%Y_%m_%d')
+
                 if not os.path.exists(total_date):
                     os.mkdir(total_date)
                 os.chdir(total_date)
@@ -73,6 +76,8 @@ def camera_process(basic_dir, Hostname, Username, Password, cnopts, \
                     #new track dir
                     dir_to_create = str(cameras[cam_path][0] + file_date[:-7])
                     path_log = file_to_folder(cameras[cam_path][0] + file_date)
+                    created_tracks.append(path_log)
+                    
                     print('\n', f' <<<<< Track {cameras[cam_path][0] + file_date} created >>>>> ', '\n')
                     #load mobile
                     if cameras[cam_path][1] in phone_list:
@@ -102,11 +107,27 @@ def camera_process(basic_dir, Hostname, Username, Password, cnopts, \
                                     sftp.get(emlid.filename, path_log + \
                                         "\\_GPS_\\ROVER\\" + emlid.filename, callback = lambda x,y: progressbar(x,y))      
                                     print(emlid.filename, '\n')  
-                    sftp.cwd(cam_path_way + "/" + Camera_n.filename)                        
+                    sftp.cwd(cam_path_way + "/" + Camera_n.filename)
+
+                # original_photos.append(path_log+"\\original\\"+file.filename)
+                # stitched_photos.append(path_log+"\\instaOne\\"+file.filename.replace(".insp", ".jpg"))
+
                 if not os.path.exists(path_log+"\\original\\"+file.filename):
                     sftp.get(file.filename, path_log+"\\original\\"+file.filename, callback = lambda x,y: progressbar(x,y))
                     print(Camera_n.filename, file.filename, file_date)
                 os.chdir(basic_dir)
+    
+    # cmd = " -inputs {} -output {} -stitch_type optflow -enable_flowstate open flowstate \
+                    # -output_size 6080x3040 -disable_cuda enable_cuda".format(" ".join(original_photos), " ".join(stitched_photos))
+    # Popen(stitch_path+stitch_runner+cmd, shell=True).wait() 
+
+    for path in created_tracks:
+        os.chdir(path+"\original")
+        for photo in os.listdir():
+            if not os.path.exists(path+"\\instaOne\\"+photo.replace(".insp", ".jpeg")):
+                cmd = " -inputs {} -output {} -stitch_type optflow -enable_flowstate open flowstate \
+                    -output_size 6080x3040 -disable_cuda enable_cuda".format(path+"\\original\\"+photo, path+"\\instaOne\\"+photo.replace(".insp", ".jpg"))
+                Popen(stitch_path+stitch_runner+cmd, shell=True).wait() 
 
 
 def main(argv):
@@ -142,11 +163,12 @@ def main(argv):
         sftp.cwd(remoteFilePath_phone)
         phone_list = [x.filename for x in sftp.listdir_attr() if x.filename in phone_dict.values()]
 
+        stitch_path = "Z:\Bike_processing_Ignat\MediaSDK_bin\\"
+        stitch_runner = "stitcherSDKDemo.exe"
 
         if argv == None:
             for cam_path in cam_list:
-                os.system('mode con: cols=10')
-                os.system("start cmd /k " + 'camera_download.py ' + cam_path.filename)
+                os.system("start cmd /c " + 'camera_download.py ' + cam_path.filename)
         
         else:
             if not os.path.exists("TRACKS"):
@@ -155,7 +177,7 @@ def main(argv):
             basic_dir = os.getcwd()
 
             print(argv)
-            camera_process(basic_dir, Hostname, Username, Password, cnopts, \
+            camera_process(stitch_path, stitch_runner, basic_dir, Hostname, Username, Password, cnopts, \
                     remoteFilePath, remoteFilePath_phone, cameras, phone_list, argv)
 
 
